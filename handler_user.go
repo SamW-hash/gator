f3f4b907b20d0536203b0a2e48ea8268c3d75494
SamW-hash <samvincentwilliams@gmail.com>
2026-02-22 17:51:27 -0800

    sqlc, generated queries, new handlers

--- handler_user.go
+++ handler_user.go
@@ -1,14 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/SamW-hash/gator/internal/database"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("You must provide a username.")
+	}
+	name := cmd.args[0]
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			os.Exit(1)
+		}
+		return err
 	}
 	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
 		return err
 	}
 	fmt.Println("User has been successfully set.")
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("You must provide a username.")
+	}
+	name := cmd.args[0]
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code == "23505" {
+				os.Exit(1)
+			}
+		}
+		return err
+	}
+	log.Printf("created user: %+v\n", user)
+	return s.cfg.SetUser(name)
 }
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/SamW-hash/gator/internal/config"
+	"github.com/SamW-hash/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,13 +21,20 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	newState := state{cfg: &cfg}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	dbQueries := database.New(db)
+	newState := state{db: dbQueries, cfg: &cfg}
 	newCommands := commands{cmdlist: make(map[string]func(*state, command) error)}
 	newCommands.register("login", handlerLogin)
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: gator <command> [args...]")
 		os.Exit(1)
 	}
+	newCommands.register("register", handlerRegister)
 	cmdName := os.Args[1]
 	cmdArgs := os.Args[2:]
 	newCommand := command{name: cmdName, args: cmdArgs}
